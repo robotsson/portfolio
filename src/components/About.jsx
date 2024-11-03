b84ba905dde4b9438c094613208a3396ae273a0b
ppettert <game-baked-jogging@duck.com>
2024-11-03 20:24:29 +0100

    small adj

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,14 +9,15 @@ function About() {
       <h2>About me</h2>
       <div className="App-about">
         <div className="App-about-text">
-
+   
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
           magnam exercitationem dicta? Mollitia ratione nisi, laborum consequuntur. 
           For more see&nbsp;
           <Link className="App-link" to="/skills">skills</Link> 
           <p></p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem dicta? Laborum consequuntur
-          labore. Have a look at my&nbsp;<Link className="App-link" to="/projects">projects</Link>  <p></p>
+          labore. Have a look at my&nbsp;
+          <Link className="App-link" to="/projects">projects</Link>  <p></p>
 
           <a href="https://robotsson.github.io/cv"
             className="App-link" 
